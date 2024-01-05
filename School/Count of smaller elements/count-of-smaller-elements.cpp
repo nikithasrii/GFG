8a50e17c4bac3031afcf6d5d7cb55029c9672d8c
nikithasrii <98526622+nikithasrii@users.noreply.github.com>
2024-01-05 22:12:38 +0530

    Added Solution - GfG to GitHub

---
+++ School/Count of smaller elements/count-of-smaller-elements.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+int countOfElements(int arr[], int n, int x);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        
+        cin >> x;
+
+        cout << countOfElements(arr, n, x) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+int countOfElements(int arr[], int n, int x) 
+{
+    int count=0;
+    int i=0;
+    while (i<n && arr[i]<=x)
+    {
+        count++;
+        i++;
+    }
+    return count;
+}
